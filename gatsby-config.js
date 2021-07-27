924edf214fd5e6e5e00dfcf15ff3849ce9023334
那可心 <nakx@raysdata.com>
2021-07-27 10:07:14 +0800

    https

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   siteMetadata: {
     title: 'AntV',
     description: 'Gatsby starter of antv theme',
-    siteUrl: 'http://antv.nargb.com/',
+    siteUrl: 'https://antv.nargb.com/',
     githubUrl: 'https://github.com/Nakexn/antv-theme.git',
     navs: [
       {
